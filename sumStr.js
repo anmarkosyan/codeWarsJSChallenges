c959a4187c1a637b16170a86ab40e914d3b7dc8d
anmarkosyan <anush.markos@gmail.com>
2020-08-06 19:58:49 +0400

    sumStr

---
+++ sumStr.js
@@ -0,0 +1,13 @@
+//https://www.codewars.com/kata/5966e33c4e686b508700002d/train/javascript
+
+
+//************************* sumStr *******************
+//Create a function that takes 2 positive integers in form of a string as an input,
+// and outputs the sum (also as a string):
+//*******************************************************
+
+function sumStr(a,b) {
+    return `${+a + +b}`;
+
+}
+console.log(sumStr(4,9));//'13'
