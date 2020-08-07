36da61d7ec9faee976a55e94578f48e649411c89
anmarkosyan <anush.markos@gmail.com>
2020-08-07 17:54:57 +0400

    get totalPrice with percents

---
+++ totalPrice.js
@@ -0,0 +1,12 @@
+// ========================== totalPrice ===================
+//Find total price in restaurant , if we have price, tipPercent, taxPercent
+//=================================================
+
+
+function totalPrice(price, tipPercent, taxPercent){
+
+    let total = price + (tipPercent / 100 ) * price + (taxPercent / 100) * price;
+    return +total.toFixed(2);
+}
+console.log(totalPrice(100, 15, 12));
+console.log(totalPrice(122, 13, 8));
