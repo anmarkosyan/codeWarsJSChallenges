bf45f65ad3daf04cc1f211810a8cb59dbed1de1e
anmarkosyan <anush.markos@gmail.com>
2020-08-08 19:33:22 +0400

    all digits are different

---
+++ diffDigit.js
@@ -0,0 +1,14 @@
+//========================== diffDigit ===============
+//write a function , and give a number , return true if all digits are different,
+//and false if some of them have same digit
+//
+
+function diffDigit(num) {
+    let first = Math.floor(num / 100);
+    let middle = Math.floor(num % 100 / 10);
+    let last = num % 10;
+    return (last !== middle && middle !== first && last !== first) ? true : false;
+}
+
+console.log(diffDigit(123))//true
+console.log(diffDigit(223))//false
