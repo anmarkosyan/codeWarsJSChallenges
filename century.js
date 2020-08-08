0a2225a9a1d32863c1df8beb645aca206aa46f68
anmarkosyan <anush.markos@gmail.com>
2020-08-08 15:45:16 +0400

    convert year to century

---
+++ century.js
@@ -0,0 +1,13 @@
+//https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097/train/javascript
+
+
+//================================= century =================================
+//The first century spans from the year 1 up to and including the year 100,
+// The second - from the year 101 up to and including the year 200, etc.
+// Given a year, return the century it is in.
+
+
+const century = year => Math.ceil(year / 100);
+
+
+console.log(century(1986));// 20
