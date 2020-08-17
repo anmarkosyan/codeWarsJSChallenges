9bba47c9c4c63403545494aa52cfdab43961f0c3
anmarkosyan <anush.markos@gmail.com>
2020-08-17 16:18:50 +0400

    array of fibanocci numbers

---
+++ fibNumbers.js
@@ -0,0 +1,16 @@
+//================== fibonacci numbers ===========================
+//write a function a return array of n  Fibonacci numbers
+
+
+function fibNum(n){
+    let arr = [0, 1];
+    for(let i = 2; i < n; i++){
+        arr[i] = arr[i - 1] + arr[i - 2];
+
+    }
+    return arr;
+}
+
+console.log(fibNum(7));//[0, 1, 1, 2, 3, 5, 8]
+console.log(fibNum(2));//[0, 1]
+console.log(fibNum(5));//[0, 1, 1, 2, 3]
