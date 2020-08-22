0f0c9c73493fbf81d5aa60d68196b925c3fa23f0
anmarkosyan <anush.markos@gmail.com>
2020-08-22 13:20:55 +0400

    added second variant of numeric table

--- numericalTable.js
+++ numericalTable.js
@@ -1,4 +1,5 @@
 //==================== numerical table ===============================
+//write a function to take a given n row and return a string of numbers 1  until 5
 //"1 1 1 1 1
 // 2 2 2 2 2
 // 3 3 3 3 3
@@ -20,3 +21,17 @@ function numericalTable(n){
 console.log(numericalTable(4));
 console.log(numericalTable(1));//'1 1 1 1 1'
 console.log(numericalTable(5));
+
+// numeric table may have 2 parameters n and m
+function numericalTable2(n, m){
+    let str = '';
+    for(let i = 1; i <= n; i++){
+        for(let j = 1; j <= m; j++){
+            if(j < m) str = str + i + ' ';
+            else str = str + i;
+        }
+        if(i <= n -1) str = str + '\n'
+    }
+    return str;
+}
+console.log(numericalTable2(3, 2)); //"1 1\n2 2\n3 3"
