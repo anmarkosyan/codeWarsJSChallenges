9f251d8b56e0251c2f8a20c635968fa8dd0fe71e
anmarkosyan <anush.markos@gmail.com>
2020-08-25 14:50:58 +0400

    power of 2

--- powerArray.js
+++ powerArray.js
@@ -2,12 +2,27 @@
 //write a function to return every number ina given array of power
 // if number is even , return square power, if odd return cube power
 
-function powerArray(arr){
-    let newArr= [];
-    for(let num of arr){
-        if(num % 2 === 0)  newArr.push(Math.pow(num, 2))
-        else newArr.push(Math.pow(num, 3))
+// function powerArray(arr){
+//     let newArr= [];
+//     for(let num of arr){
+//         if(num % 2 === 0)  newArr.push(Math.pow(num, 2))
+//         else newArr.push(Math.pow(num, 3))
+//     }
+//     return newArr;
+// }
+// console.log(powerArray([1, 4, 3, 5, 2]));//[1, 16, 27, 125, 4]
+
+//write a function a find a power of given n, and collect it into array
+//count of power started with 0;
+
+function powerOfTwo(n){
+    let arr = [];
+    let p = 0;
+    while(p <= n){
+        arr.push(Math.pow(2, p));
+        p++;
     }
-    return newArr;
+return arr;
 }
-console.log(powerArray([1, 4, 3, 5, 2]));//[1, 16, 27, 125, 4]
+console.log(powerOfTwo(2));//[1, 2, 4]
+console.log(powerOfTwo(4));//[1, 2, 4, 8, 16]
