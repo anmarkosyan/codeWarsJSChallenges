4a1a74c566c66e44c38e54789a2266909f7ceb17
anmarkosyan <anush.markos@gmail.com>
2020-08-30 14:07:27 +0400

    convert number to diff base

--- convertToBinary.js
+++ convertToBinary.js
@@ -10,3 +10,14 @@ console.log(convertToBinary(3));//11
 console.log(convertToBinary(2));//10
 
 
+//========== convert numbers ========
+const convertNumbers = num => {
+    let decimal = +num.toString();
+    let binary = +num.toString(2);
+    let octal = +num.toString(8);
+    let hex = num.toString(16);
+    return [decimal, binary, octal, hex];
+}
+console.log(convertNumbers(5));//[ 5, 101, 5, '5' ]
+console.log(convertNumbers(12))//[ 12, 1100, 14, 'c' ]
+
