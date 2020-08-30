3d04ec114e971837cbbc498485c1a0253921400c
anmarkosyan <anush.markos@gmail.com>
2020-08-30 13:42:09 +0400

    convert number to Binary num

---
+++ convertToBinary.js
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/59fca81a5712f9fa4700159a/train/javascript
+
+//========================= convert to Binary ===========================
+//convert non-negative integer to Binary format
+
+const convertToBinary = n => +n.toString(2);
+
+
+console.log(convertToBinary(3));//11
+console.log(convertToBinary(2));//10
+
+
