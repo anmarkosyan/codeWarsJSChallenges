ad80ee162a0f61d97e058e2a89463d9f884238ea
anmarkosyan <anush.markos@gmail.com>
2020-09-01 12:45:46 +0400

    added how to count in the given range prime numbers

--- isPrime.js
+++ isPrime.js
@@ -4,15 +4,30 @@
 //=======================================================
 
 
-function isPrime(n){
-    for(let i = 2; i < n; i++){
+const isPrime = n => {
+    if(n < 2) return false;
+    //if we have big number so we should i <= n / 2 , for fast solution
+    for(let i = 2; i <= n / 2; i++){
         if(n % i === 0){
             return false;
         }
     }
-    return n > 1;
+    return true;
 }
 console.log(isPrime(2));// true
 console.log(isPrime(1));// false
 console.log(isPrime(30));//false
 console.log(isPrime(5));// true
+console.log(isPrime(2567));// false
+
+const countOfPrimeNumbers = (n, m) => {
+    let count = 0;
+    for(let i = n; i <= m; i++){
+        if(isPrime(i)) count++;
+    }
+    return count;
+
+}
+
+console.log(countOfPrimeNumbers(10, 90));// 20
+console.log(countOfPrimeNumbers(1, 10));// 4
