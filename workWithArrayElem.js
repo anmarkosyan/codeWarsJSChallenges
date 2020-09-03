ee0538cde9edba4730cc6d98980e035ab388a929
anmarkosyan <anush.markos@gmail.com>
2020-09-03 19:22:47 +0400

    added count of square and count of positive pairs

--- workWithArrayElem.js
+++ workWithArrayElem.js
@@ -39,3 +39,34 @@ const countIntegers = arr => {
 return count;
 }
 console.log(countIntegers([0, 4.1, 'f', true, 1, false, 'h', -3]));//3
+
+//================== count of squares ===================
+const countOfSquares = arr => {
+    let count = 0;
+    for(let elem of arr){
+        if(typeof elem === 'number' && Math.sqrt(elem) % 1 === 0) count++;
+    }
+    return count;
+
+}
+console.log(countOfSquares([0, 25, 'f', true, 9, 7, false, 'h']));//3
+
+//=================== count of positive pairs ===============
+const countOfPositivePairs = arr => {
+    let count = 0;
+    let index = [];
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] > 0 ) index.push(i);
+    }
+    for(let j = 0; j < index.length; j++){
+        let first = index[j];
+        let second = index[j + 1];
+        if(second - first === 1) count++;
+    }
+return count;
+}
+console.log(countOfPositivePairs([0, 25, 3, -2, 5]));//1 => 25 and 3
+console.log(countOfPositivePairs( [1,7,4,-1]));//2 => 1,7 and 7,4
+
+
+
