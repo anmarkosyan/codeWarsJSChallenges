138a1930bcca07de6c82823af70802553a4f92b9
anmarkosyan <anush.markos@gmail.com>
2020-09-03 12:54:43 +0400

    added some cases

--- fibNumbers.js
+++ fibNumbers.js
@@ -4,6 +4,8 @@
 
 function fibNum(n){
     let arr = [0, 1];
+    if(n === 1) return [arr[0]];
+    if(n <= 0) return [];
     for(let i = 2; i < n; i++){
         arr[i] = arr[i - 1] + arr[i - 2];
 
@@ -14,3 +16,5 @@ function fibNum(n){
 console.log(fibNum(7));//[0, 1, 1, 2, 3, 5, 8]
 console.log(fibNum(2));//[0, 1]
 console.log(fibNum(5));//[0, 1, 1, 2, 3]
+console.log(fibNum(1));//[0]
+
