072fbc2e265e5628ae91e8d948c6e8895b473e69
anmarkosyan <anush.markos@gmail.com>
2020-09-11 17:59:30 +0400

    sort method

---
+++ sortMethod.js
@@ -0,0 +1,85 @@
+//========== Two Oldest Ages ====================
+
+function twoOldestAges(ages){
+    ages.sort((a, b) => b -a);
+    return [ages[1], ages[0]]
+
+}
+console.log(twoOldestAges( [1, 2, 10, 8]));//[8, 10]
+
+//==================== Largest pair sum in array ===========
+function largestPairSum(numbers){
+    numbers.sort((a, b) => b - a);
+    return numbers[0] + numbers[1]
+
+}
+console.log(largestPairSum([-10, -8, -16, -18, -19]));// -18
+
+//=======================Sum of two lowest positive integers ==============
+
+function sumTwoSmallestNumbers(numbers) {
+    numbers.sort((a, b) => a - b);
+    return numbers[0] + numbers[1];
+}
+console.log(sumTwoSmallestNumbers([5, 8, 12, 19, 22]));//13
+
+//=================== Find Maximum and Minimum Values of a List =====
+
+
+const min = function(list){
+    //1 way
+  // let minNum = list[0];
+  // for(let i = 1; i < list.length; i++ ){
+  //   if(list[i] < minNum){
+  //     minNum = list[i];
+  //   }
+  // }
+  //   return minNum;
+    //2 way
+    //return Math.min(...list);
+    //3 way
+    list.sort((a, b) => a - b);
+    return list[0]
+}
+console.log(min([-52, 56, 30, 29, -54, 0, -110]));//-110
+
+const max = function(list){
+    //1 way
+//   let maxNum = list[0];
+//   for(let i = 1; i < list.length; i++){
+//     if(list[i] > maxNum){
+//       maxNum = list[i];
+//     }
+//   }
+//   return maxNum;
+    //2 way
+    //return Math.max(...list)
+    //3 way
+    list.sort((a, b) => b - a);
+    return list[0]
+}
+console.log(max([4,6,2,1,9,63,-134,566]));//566
+
+
+//==================== Sort array by string length ==============
+
+function sortByLength (array) {
+    return array.sort((a, b) => a.length - b.length);
+}
+console.log(sortByLength(["Beg", "Life", "I", "To"]));//[ 'I', 'To', 'Beg', 'Life' ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
