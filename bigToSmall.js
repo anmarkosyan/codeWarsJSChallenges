40ebd4c6a9068748990b0367efb39c9bc59bd18a
anmarkosyan <anush.markos@gmail.com>
2020-09-11 15:24:50 +0400

    numbers from big to small using sort() join()

--- bigToSmall.js
+++ bigToSmall.js
@@ -5,9 +5,12 @@
 // Finally, use the separator ">" to connect the elements into a string.
 
 const bigToSmall = arr => {
-    let res = [];
-    return res.concat(...arr).sort().reverse().join('>');
+    //if we want to sort numbers we should use parameters within sort() method
+    //numArray.sort((a, b) => a - b); // For ascending sort
+    //numArray.sort((a, b) => b - a); // For descending sort
+    return [].concat(...arr).sort((a, b) => b - a).join('>');
 
 }
 console.log(bigToSmall([[1,3,5],[2,4,6]]));//"6>5>4>3>2>1"
-console.log(bigToSmall([[1,2],[3,4],[5,6]]));//6>5>4>3>2>1"
+console.log(bigToSmall([[1,2],[3,4],[5,6]]));//"6>5>4>3>2>1"
+console.log(bigToSmall([[20, 12, 3, 2, 66], [4, 5, 11]]));//"66>20>12>11>5>4>3>2" => (with sort() =>66>5>4>3>20>2>12>11)
