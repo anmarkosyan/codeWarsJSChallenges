c75084dabed466e486ff0c833f0eece67e77c792
anmarkosyan <anush.markos@gmail.com>
2020-09-11 10:47:32 +0400

    all letters only one time, delete duplicates

--- arrDuplicates.js
+++ arrDuplicates.js
@@ -25,5 +25,14 @@ const res3 = arr3.filter((el, i) => i === arr3.indexOf(el) && i !== arr3.lastInd
 console.log(res3);
 
 
+//===================== return letters only one time and sorted it,delete duplicates =========================
+//Take 2 strings s1 and s2 including only letters from ato z.
+//Return a new sorted string, the longest possible, containing distinct letters,
+// each taken only once - coming from s1 or s2.//conde wars//
 
+const longest = (str1, str2) => {
+    let res = (str1 + str2).split('');
+    return res.filter((el, i) => i === res.indexOf(el)).sort().join('');
+}
+console.log(longest("aretheyhere", "yestheyarehere"));//"aehrsty"
 
