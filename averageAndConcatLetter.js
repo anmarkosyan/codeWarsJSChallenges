a30e1a9e569f07093b65bbbbcd3f4a99a3fc4f13
anmarkosyan <anush.markos@gmail.com>
2020-09-13 18:41:34 +0400

    i find solution of this task with 2 way

--- averageAndConcatLetter.js
+++ averageAndConcatLetter.js
@@ -5,11 +5,18 @@
 // Create a single string with the characters and return it as a[1] while maintaining the original order.
 
 function mean(arr){
-    let num = [];
-    let res = arr.filter(el => !isNaN(el)).forEach(el => num.push(+el));
-    return  [num.reduce((sum, item) => sum + item, 0) / num.length, arr.filter(el => isNaN(el)).join('')]
+    //1 way
+    // let str = '', sum = 0;
+    // for(let el of arr){
+    //     if(!isNaN(el)) sum += +el;
+    //     else str += el;
+    // }
+    //     return [sum / 10, str]
+
+    //2 way
+    return  [arr.filter(el => !isNaN(el)).reduce((sum, item) => sum + +item, 0) / 10, arr.filter(el => isNaN(el)).join('')]
+
 }
 console.log(mean(["u", "6", "d", "1", "i", "w", "6", "s", "t", "4", "a", "6", "g", "1", "2", "w", "8", "o", "2", "0"]));
 // [3.6, "udiwstagwo"]
 
-//!!!!!! work but something  wrong with res, because I dont use it
