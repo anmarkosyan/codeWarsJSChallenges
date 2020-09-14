727e72b777cce468c5b5e6986d2a12ca8fce1aa9
anmarkosyan <anush.markos@gmail.com>
2020-09-14 16:33:22 +0400

    added how to find maxS and maxP of a rectangle

--- mapMethod.js
+++ mapMethod.js
@@ -24,3 +24,18 @@ let arr4 =  [-37, -8, 56.3, 0, 18.9];
 let res2 = arr4.map(el => el + '');//String(el)
 console.log(res2);//[ '-37', '-8', '56.3', '0', '18.9' ]
 console.log(arr4)
+
+
+//=============== find  Maximum area of a rectangle =========
+
+const maxS = arr => {
+    return arr.length === 0 ? null : Math.max(...arr.map(el => el[0] * el[1]));
+}
+console.log(maxS([[3, 4], [2, 1], [8, 3]]));// 24
+console.log(maxS([]));//null
+
+//============ find maximum perimeter of rectangle ======
+const maxP = arr => {
+    return arr.length === 0 ? null : Math.max(...arr.map(el => 2 * (el[0] + el[1])));
+}
+console.log(maxP([[3, 4], [2, 1], [8, 3]]));//22
