0f5fbfbe3aa98f4557fc63bc3dbc02b428cffbb7
anmarkosyan <anush.markos@gmail.com>
2020-09-16 12:52:28 +0400

    second way of solve problem

--- squareDigits.js
+++ squareDigits.js
@@ -4,11 +4,13 @@
 //write a function to square every digit of a number and concat it
 
 function squareDigits (num){
-    let str = num + '';
-    let res = '';
-    for(let el of str){
-        res += el ** 2;
-    }
-    return +res;
+    // let str = num + '';
+    // let res = '';
+    // for(let el of str){
+    //     res += el ** 2;
+    // }
+    // return +res;
+    //2 way
+    return parseInt((num + '').split('').map(el => Math.pow(el, 2)).join(''));
 }
 console.log(squareDigits(9119));//811181
