1a8687d497b452db5c764c9e283bcb7ffdf24fea
anmarkosyan <anush.markos@gmail.com>
2020-09-19 16:29:35 +0400

    added new task using regExp

--- regExp.js
+++ regExp.js
@@ -11,6 +11,17 @@ console.log(lowerCaseCount("ABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~"));//0
 
 
 
+//================== Fix string case =================
+//https://www.codewars.com/kata/5b180e9fedaa564a7000009a/train/javascript
+
+function solve(s){
+    let regExp = s.match(/[A-Z]/g);
+    return regExp !== null && regExp.length > s.length / 2 ? s.toUpperCase() : s.toLowerCase()
+}
+console.log(solve("code"));//'code'
+console.log(solve("CODe"));//CODE
+console.log(solve('COde'));//code
+
 
 
 
