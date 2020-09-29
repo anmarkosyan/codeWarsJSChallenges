295fa63213478c44e93567218eb50aa0cc57012a
anmarkosyan <anush.markos@gmail.com>
2020-09-29 13:10:43 +0400

    how to swap tail using split or slice method

--- sliceMethod.js
+++ sliceMethod.js
@@ -32,3 +32,24 @@ console.log(shortenToDate("Friday May 2, 9am"));//Friday May 2"
 console.log(shortenToDate("Tuesday January 29, 10pm"));//Tuesday January 29
 
 
+//============================ Tail Swap ==============================
+//
+//You'll be given a list of two strings, and each will contain exactly one colon (":") in the middle (but not at beginning or end).
+//The length of the strings, before and after the colon, are random.
+// Your job is to return a list of two strings (in the same order as the original list), but with the characters after each colon swapped.
+
+
+function tailSwap(arr) {
+    //#1
+    // let first = arr[0].slice(arr[0].indexOf(':'));
+    // let second = arr[1].slice(arr[1].indexOf(':'));
+    // return [arr[0].slice(0, arr[0].indexOf(':')) + second,
+    //         arr[1].slice(0, arr[1].indexOf(":")) + first];
+    //#2
+    let a =  arr[0].split(':');
+    let b = arr[1].split(':');
+    return [`${a[0]}:${b[1]}`, `${b[0]}:${a[1]}`]
+}
+console.log(tailSwap(['abc:123', 'cde:456']));//['abc:456', 'cde:123']
+console.log(tailSwap(['a:12345', '777:xyz']));//['a:xyz', '777:12345']
+
