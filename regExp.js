34d44b12b7f10c7755539ea684756912d45a1946
anmarkosyan <anush.markos@gmail.com>
2020-09-29 19:38:54 +0400

    some new tasks

--- regExp.js
+++ regExp.js
@@ -64,7 +64,7 @@ const FilterString = function(value) {
     // let regExp = value.match(/\d/g).join('');
     // return parseInt(regExp);
     //2 faster
-    return parseInt(value.replace(/\D/g,''))
+    return parseInt(value.replace(/\D/g,''))// or return +value.replace(/\D/g, '');
 
 }
 console.log(FilterString("a1b2c3"));//123
@@ -83,6 +83,70 @@ function noSpace(x){
 
 }
 console.log(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'));//8j8mBliB8gimjB8B8jlB
+
+//======================== Vowel remover ==============
+
+function shortcut(string){
+    return string.replace(/[aeiou]/g, '')
+
+}
+console.log(shortcut('codewars'));//cdwrs
+console.log(shortcut('how are you today?'));//hw r y tdy?
+
+
+//==================== Polish alphabet=========================
+//There are 32 letters in the Polish alphabet: 9 vowels and 23 consonants.
+// Your task is to change the letters with diacritics:
+//ą -> a,
+// ć -> c,
+// ę -> e,
+// ł -> l,
+// ń -> n,
+// ó -> o,
+// ś -> s,
+// ź -> z,
+// ż -> z
+
+function correctPolishLetters (str) {
+    let polish = {
+        ą: 'a',
+        ć: 'c',
+        ę: 'e',
+        ł: 'l',
+        ń: 'n',
+        ó: 'o',
+        ś: 's',
+        ź: 'z',
+        ż: 'z'
+    };
+    //#1 time:slow O(n^2)
+    //return str.split('').map(el => polish[el] || el).join('');
+    //#2 time: O(1)
+    return str.replace(/[ąćęłńóśźż]/g, el => polish[el])
+}
+console.log(correctPolishLetters("Jędrzej Błądziński"));//Jedrzej Bladzinski
+console.log(correctPolishLetters("Lech Wałęsa"));//Lech Walesa
+
+
+//==================== fake binary =================
+//https://www.codewars.com/kata/57eae65a4321032ce000002d/train/javascript
+function fakeBin(x){
+    let binary = {0:0, 1:0, 2:0, 3:0, 4:0, 5:1, 6:1, 7:1, 8:1, 9:1};
+    return x.replace(/[0-9]/g, num => binary[num])
+}
+console.log(fakeBin('45385593107843568'));//01011110001100111
+
+
+//======================= remove all exclamation marks from the end =========
+
+function remove(s){
+    return s.replace(/!+$/g, '')
+
+}
+console.log(remove("Hi!!!"));//Hi
+console.log(remove("Hi! Hi!"));//Hi! Hi
+
+
 
 
 
