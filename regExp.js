efe72ea8c9fb29955227b53e7796265521ad31fb
anmarkosyan <anush.markos@gmail.com>
2020-09-30 14:25:32 +0400

    start with vowel

--- regExp.js
+++ regExp.js
@@ -146,6 +146,25 @@ function remove(s){
 console.log(remove("Hi!!!"));//Hi
 console.log(remove("Hi! Hi!"));//Hi! Hi
 
+//========================== Start with a Vowel =============================
+//https://www.codewars.com/kata/5a02e9c19f8e2dbd50000167/train/javascript
+//Create a function that takes any sentence and redistributes the spaces (and adds additional spaces if needed)
+//so that each word starts with a vowel. The letters should all be in the same order but every vowel
+//in the sentence should be the start of a new word. The first word in the new sentence may start without a vowel.
+//It should return a string in all lowercase with no punctuation (only alphanumeric characters).
+
+function vowelStart(str){
+
+    return str.toLowerCase()
+        .replace(/[_, !-]/g, '')// replace(/[\W_]/g, '')
+        .replace(/[aeiou]/g, el => ' ' + el)
+        .trim();
+
+}
+console.log(vowelStart('It is beautiful weather today!'));//'it isb e a ut if ulw e ath ert od ay'
+console.log(vowelStart('oranges, apples, melon, pineapple'));//'or ang es appl esm el onp in e appl e'
+console.log(vowelStart('my number is 0208-533-2325'));//'myn umb er is02085332325'
+console.log(vowelStart('under_score'));//'und ersc or e'
 
 
 
