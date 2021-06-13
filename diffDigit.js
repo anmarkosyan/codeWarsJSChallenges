0be0f4178735e7d2ae4f157d3be702e414ae3746
anmarkosyan <anush.markos@gmail.com>
2021-06-13 14:44:17 +0400

    some changes

--- diffDigit.js
+++ diffDigit.js
@@ -4,11 +4,11 @@
 //
 
 function diffDigit(num) {
-    let first = Math.floor(num / 100);
-    let middle = Math.floor(num % 100 / 10);
-    let last = num % 10;
-    return (last !== middle && middle !== first && last !== first) ? true : false;
+  let first = Math.floor(num / 100);
+  let middle = Math.floor((num % 100) / 10);
+  let last = num % 10;
+  return last !== middle && middle !== first && last !== first;
 }
 
-console.log(diffDigit(123))//true
-console.log(diffDigit(223))//false
+console.log(diffDigit(123)); //true
+console.log(diffDigit(223)); //false
